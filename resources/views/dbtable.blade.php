6e8d0f29d19342c1a58583d05ff6649e9f55bb51
Vladimir <vladimir.solomka@gmail.com>
2021-04-04 22:28:10 +0300

    Lesson 10 HomeTask: Laravel - installation, routes, tables from database

---
+++ resources/views/dbtable.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="/css/common.css" />
+</head>
+<body>
+@if ($data)
+<table class="dbtable">
+    <tr>
+        @foreach ($data[0] as $field => $value)
+        <th>{{ $field }}</th>
+        @endforeach
+    </tr>
+    @foreach ($data as $row)
+        <tr>
+            @foreach ($row as $col)
+            <td>{{ $col }}</td>
+            @endforeach
+        </tr>
+    @endforeach
+</table>
+@endif
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DBTableController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dbtable/{name}', [DBTableController::class, 'display']);
