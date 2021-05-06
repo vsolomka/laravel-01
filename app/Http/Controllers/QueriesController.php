cbe5a376bbfded37345cf0d276165882dff4a9cd
Vladimir <vladimir.solomka@gmail.com>
2021-05-06 12:59:30 +0300

    Lesson 18 Hometask: Swagger setup

--- app/Http/Controllers/QueriesController.php
+++ app/Http/Controllers/QueriesController.php
@@ -6,6 +6,48 @@ use App\Models\Queries\PaidOrders;
 use App\Models\Queries\OrdersByCustomer;
 use App\Models\Queries\AmountsForCustomers;
 
+/**
+ * Class QueriesController
+ * @package App\Http\Controllers
+ *
+ * @OA\Get(
+ *     path="/query/paid-orders",
+ *     summary="Orders that have been already paid (HTML)",
+ *     tags={},
+ *
+ *     @OA\Response(
+ *         response=200,
+ *         description="Success",
+ *     )
+ * )
+ * @OA\Get(
+ *     path="/query/amounts-for-customers",
+ *     summary="Amount per customer, for all customers (HTML)",
+ *     tags={},
+ *
+ *     @OA\Response(
+ *         response=200,
+ *         description="Success",
+ *     )
+ * )
+ * @OA\Get(
+ *     path="/query/orders-by-customer/{id}",
+ *     summary="Orders list for given customer (HTML)",
+ *     @OA\Parameter(
+ *         description="ID of customer",
+ *         in="path",
+ *         name="id",
+ *         required=true,
+ *         example="1"
+ *     ),
+ *     tags={},
+ *
+ *     @OA\Response(
+ *         response=200,
+ *         description="Success",
+ *     )
+ * )
+ */
 
 class QueriesController extends Controller
 {
